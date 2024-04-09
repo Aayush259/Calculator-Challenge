44e7d11db02f5b82357c385abc0a3843f5df0012
Aayush259 <aayushk4980@gmail.com>
2024-04-09 23:16:31 +0530

    Added functionality of calculator

---
+++ scripts/script.js
@@ -0,0 +1,108 @@
+// Getting calculate button.
+const CalculateBtn = document.getElementById('calculate-button');
+
+// Getting all input buttons.
+const InputButtons = document.querySelectorAll(`button`);
+
+// Array of operators in calculator app.
+const ArrayOfOperators = [`+`, `-`, `x`, `/`, `.`];
+
+// This function displays result.
+const ShowResult = (result) => {
+    document.getElementById('output-box').value = result;
+}
+
+/*
+    This function calculates the result.
+*/
+const Calculate = () => {
+
+    // Getting output box.
+    const OutputBox = document.getElementById('output-box');
+
+    // Getting expression which is to be calculated.
+    let expression = OutputBox.value;
+
+    // If there is nothing in expression, then do nothing.
+    if (expression === ``) {
+        return;
+    }
+
+    // If the first and last characters of the expression are operators, then remove them from expression.
+    if (ArrayOfOperators.includes(expression[0])) {
+        expression = expression.slice(1);
+    }
+    if (ArrayOfOperators.includes(expression[expression.length - 1])) {
+        expression = expression.slice(0, -1);
+    }
+
+    // Replace all the `x` from expression with `*`.
+    const Regex = /x/g;
+    expression = expression.replace(Regex, '*');
+
+    // Calculate result.
+    const Result = eval(expression);
+    
+    // Display result.
+    ShowResult(Result);
+}
+
+const TakeInput = (text) => {
+
+    // Getting output box.
+    const OutputBox = document.getElementById('output-box');
+
+    // If the output box value if Error or Infinity then replace it with current text, else if the last character of the input is similar to the current input character, then update the last character of input with the current input character. Else, append the current character in the output box.
+    if (OutputBox.value === `Error` || OutputBox.value === `Infinity`) {
+        OutputBox.value = text;
+    } else if (ArrayOfOperators.includes(OutputBox.value.slice(-1)) && ArrayOfOperators.includes(text)) {
+        OutputBox.value = `${OutputBox.value.slice(0, -1)}${text}`;
+    } else {
+        OutputBox.value += text;
+    };
+
+}
+
+/*
+    This function deletes the last character of the output box value.
+*/
+const Delete = () => {
+
+    // Getting Output box.
+    const OutputBox = document.getElementById('output-box');
+
+    // Deleting the last character of the output box.
+    OutputBox.value = OutputBox.value.slice(0, -1);
+}
+
+/*
+    This function clears the complete input.
+*/
+const Reset = () => {
+    document.getElementById('output-box').value = ``;
+}
+
+/*
+    Adding click event listener to each button so that when any of the button is clicked, then respective task will be performed.
+*/
+InputButtons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        // If the button is delete, then call Delete() function.
+        if (button.innerText === `DEL`) {
+            Delete();
+        }
+        // If button is reset, then call Reset() function.
+        else if (button.innerText === `RESET`) {
+            Reset();
+        }
+        // If button is equal button, then calculate and show result.
+        else if (button.innerText === `=`) {
+            Calculate();
+        }
+        // Else add the text content of the button as input.
+        else {
+            TakeInput(button.innerText);
+        }
+    })
+})
